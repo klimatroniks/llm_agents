83ef0e72b146f97b92cea80b026621f93512c069
Klimentiy Yudin <klimentiyyudin@mail.ru>
2026-01-28 22:24:52 +0300

    feat: add requrements-dev and toml

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ def main():
         print("\n--- RESULT ---")
         print(result)
     finally:
-        browser.close()
+        browser.close()  # TODO: можно не выключать браузер
 
 
 if __name__ == "__main__":
